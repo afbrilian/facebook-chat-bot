e8ac83bf8d26778ac07a53f7aca76038d7e6e3f3
Afif Fahmi Brilian <brilian@huubap.com>
2019-07-11 21:49:40 +0700

    better observable handling

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,16 +3,22 @@ import { HttpClientService } from './http-client.service';
 import { FbMessage } from './fb/fb-responses';
 import { MemoryService } from './memory.service';
 import { History } from './app.model';
+import { filter, map } from 'rxjs/operators';
 
 @Injectable()
 export class AppService {
   constructor(private readonly memoryService: MemoryService, private readonly httpClientService: HttpClientService) {}
 
   send(fbMessage: FbMessage): void {
-    this.memoryService.getById(fbMessage.sender.id).subscribe((history: History) => {
-      history = history ? history : this.memoryService.createHistory(fbMessage);
-      this.message(history, fbMessage);
-    });
+    this.memoryService
+      .getById(fbMessage.sender.id)
+      .pipe(
+        filter((history) => !history),
+        map((history) => (history ? history : this.memoryService.createHistory(fbMessage)))
+      )
+      .subscribe((history: History) => {
+        this.message(history, fbMessage);
+      });
   }
 
   message(history: History, fbMessage: FbMessage): void {
