f0cbf0d22b53cf4a576ed2ee28ee6dbf2c6eda45
Afif Fahmi Brilian <brilian@huubap.com>
2019-07-11 21:13:32 +0700

    create init auto reply bot

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Req, Res, Post } from '@nestjs/common';
-import { AppService } from './app.service';
 import { Request, Response } from 'express';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
@@ -8,28 +8,21 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'facebook chat bot WebHook';
   }
 
   @Get('webhook')
   getWebhook(@Req() req: Request, @Res() res: Response): void {
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = '<YOUR_VERIFY_TOKEN>';
+    const VERIFY_TOKEN = 'testBot_verify_token>';
 
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
 
-    // Checks if a token and mode is in the query string of the request
     if (mode && token) {
-      // Checks the mode and token sent is correct
       if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
         res.status(200).send(challenge);
       } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
         res.sendStatus(403);
       }
     }
@@ -37,22 +30,17 @@ export class AppController {
 
   @Post('webhook')
   postWebHook(@Req() req: Request, @Res() res: Response): void {
-    let body = req.body;
-
-    // Checks this is an event from a page subscription
+    const body = req.body;
     if (body.object === 'page') {
-      // Iterates over each entry - there may be multiple if batched
-      body.entry.forEach(function(entry) {
-        // Gets the message. entry.messaging is an array, but
-        // will only ever contain one message, so we get index 0
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
+      body.entry.forEach((entry) => {
+        const webhookEvent = entry.messaging[0];
+        if (webhookEvent && webhookEvent.message && webhookEvent.message.text) {
+          console.log(webhookEvent);
+          this.appService.send(webhookEvent.sender.id, { text: webhookEvent.message.text });
+        }
       });
-
-      // Returns a '200 OK' response to all requests
       res.status(200).send('EVENT_RECEIVED');
     } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
       res.sendStatus(404);
     }
   }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
+import { AxiosRequestConfig } from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService) {}
+
+  send(recipientId: string, message: any): void {
+    const body = {
+      recipient: { id: recipientId },
+      message
+    };
+
+    const config: AxiosRequestConfig = {
+      headers: { 'Content-Type': 'application/json' },
+      params: { access_token: process.env.PAGE_ACCESS_TOKEN }
+    };
+
+    this.httpService.post('https://graph.facebook.com/v3.3/me/messages', body, config).subscribe();
   }
 }
