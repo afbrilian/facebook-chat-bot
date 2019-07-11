af100e3648d0ec3db00fa88bf935948ca44efed9
Afif Fahmi Brilian <brilian@huubap.com>
2019-07-11 21:34:47 +0700

    define the model and structure based on requirement

---
+++ src/app.model.d.ts
@@ -0,0 +1,20 @@
+import { ChatState } from "./app.state";
+
+interface History {
+  id: string;
+  data: UserData;
+  state: ChatState;
+}
+
+interface UserData {
+  firstName: string;
+  birthDate: Date;
+}
+
+interface Message {
+  id: string;
+  historyId: string;
+  text: string;
+}
+
+export { ChatState, History, UserData, Message };
---
+++ src/app.state.ts
@@ -0,0 +1,7 @@
+export enum ChatState {
+  INIT = 'InitChatEvent',
+  HI = 'HiChatEvent',
+  FIRST_NAME = 'FirstNameChatEvent',
+  BIRTH_DATE = 'BirthDateChatEvent',
+  DONE = 'DoneEvent',
+}
