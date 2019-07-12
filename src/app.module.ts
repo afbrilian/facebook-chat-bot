b8eb3206552876b91ca6818bcac9960d2d201d7b
--global <af.brilian@gmail.com>
2019-07-12 15:24:01 +0700

    register module and handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpClientService } from './http-client.service';
 import { MemoryService } from './memory.service';
+import { InitChatEventHandler } from './event';
+import { CqrsModule } from '@nestjs/cqrs';
+
+const handler = [InitChatEventHandler];
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CqrsModule],
   controllers: [AppController],
-  providers: [AppService, HttpClientService, MemoryService],
+  providers: [AppService, HttpClientService, MemoryService, ...handler]
 })
 export class AppModule {}
